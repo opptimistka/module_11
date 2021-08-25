92a4bff40086aca6f8927e3e48d953c9ffdba151
Liudmila Apet <Liudmila_Apet@epam.com>
2021-08-25 15:08:12 +0300

    malformed config fix

--- test/configuration/protractor-config.js
+++ test/configuration/protractor-config.js
@@ -1,39 +1,41 @@
 const path = require('path');
-const {
-    browser
-} = require('protractor');
+const yargs = require('yargs').argv;
+// const reporter = require('cucumber-html-reporter');
+
+// const reportOtions = {
+//     theme: 'bootstrap',
+//     jsonFile: path.join(__dirname, '../reports/report.json'),
+//     output: path.join(__dirname, '../reports/cucumber-report.html'),
+//     reportSuitsAsSCenarios: true
+// };
+
 exports.config = {
     allScriptsTimeout: 10000,
     getPageTimeout: 10000,
     specs: [path.resolve('./test/features/**/*.feature')],
-    // specs: ['../features/epamcom.feature'],
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
     capabilities: {
+        shardTestFiles: yargs.instances > 1,
+        maxInstances: yargs.instances || 1,
         browserName: 'chrome',
         chromeOptions: {
-            args: ['--no-sandbox', '--start-fullscreen']
+            args: ['--no-sandbox']
         },
-        'goog:chromeOptions': {
-            w3c: false
-        },
-        edgeOptions: {
-            w3c: false
-        },
-        disableChecks: true,
-        directConnect: true,
-        cucumberOpts: {
-            require: [path.resolve('./test/step_definitions/**/*.js')],
-            //            ignoreUncaughtExceptions: true,
-            //            format: ['json:./test/reports/report.json'],
-            tags: '@epam'
-        },
-        onPrepare: () => {
-            browser.waitForAngularEnabled(false);
-        },
-        onPrepare: () => {
-            //            browser.manage().window().maximize()
-            browser.manage().window().setSize(1920, 1080);
-        }
+    },
+    disableChecks: true,
+    directConnect: true,
+    cucumberOpts: {
+        require: [path.resolve('./test/step_definitions/**/*.js')],
+        ignoreUncaughtExceptions: true,
+        format: ['json:./test/reports/report.json', './node_modules/cucumber-pretty'],
+        tags: yargs.tags || '@smoke'
+    },
+    onPrepare: function () {
+        browser.waitForAngularEnabled(false);
+        return browser.manage().window().maximize();
+    },
+    afterLaunch: () => {
+        return reporter.generate(reportOtions);
     }
-}
+};
